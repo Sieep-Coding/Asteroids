2959a87ee8fefbe87703c6ea7eddd1d9d7e9e5a7
Sieep-Coding <nstamb6@gmail.com>
2024-06-18 12:03:38 -0400

    player header file :-)

---
+++ player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "raylib.h"
+
+typedef struct Player {
+    Vector2 position;
+    Vector2 velocity;
+    float rotation;
+    float speed;
+    bool active;
+
+} Player;
+
+void InitPlayer(Player *player);
+void UpdatePlayer(Player *player);
+void DrawPlayer(Player *player);
+
+
+#endif
